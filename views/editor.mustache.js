f50f08ce3de7decd07cc61c58e31b130942cb9b9
Matthew Page <matthewnpage@gmail.com>
2011-06-05 09:32:47 -0700

    set vf syntax highlighting to use CodeMirror htmlmixed mode

--- views/editor.mustache.js
+++ views/editor.mustache.js
@@ -5,10 +5,22 @@ licensed under the MIT license: http://www.opensource.org/licenses/mit-license.p
 */
 module.exports = {
   stylesheets: function() {
-    return ["js/libs/codemirror/lib/codemirror.css", "css/editor.css", "css/apex.css"];
+    return ["js/libs/codemirror/lib/codemirror.css",
+            "js/libs/codemirror/mode/xml/xml.css",
+            "js/libs/codemirror/mode/css/css.css",
+            "js/libs/codemirror/mode/javascript/javascript.css",
+            "css/editor.css",
+            "css/apex.css"];
   },
   scripts: function(){
-    return ["js/libs/codemirror/lib/codemirror.js", "js/apex.js", "js/editortabs.js", "js/docaccordion.js"];
+    return ["js/libs/codemirror/lib/codemirror.js",
+            "js/libs/codemirror/mode/htmlmixed/htmlmixed.js",
+            "js/libs/codemirror/mode/xml/xml.js",
+            "js/libs/codemirror/mode/css/css.js",
+            "js/libs/codemirror/mode/javascript/javascript.js",
+            "js/apex.js",
+            "js/editortabs.js",
+            "js/docaccordion.js"];
   }
 };
 
